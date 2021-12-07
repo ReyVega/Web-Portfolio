ffc3511ff4367a07bf77932d2e33b3265433539e
Rey Vega <reynaldovega2010@hotmail.com>
2021-12-07 17:42:19 -0600

    Adds CSS layout for menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+        <header className="l-header" id="header">
+            <nav className="nav bd-container">
+                <a href="#" className="nav__logo">Rey Vega</a>
+
+                <div className="nav__menu" id="nav-menu">
+                    <ul className="nav__list">
+                        <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
+                        <li className="nav__item"><a href="#about" className="nav__link">About</a></li>
+                        <li className="nav__item"><a href="#services" className="nav__link">Services</a></li>
+                        <li className="nav__item"><a href="#portfolio" className="nav__link">Portfolio</a></li>
+                        <li className="nav__item"><a href="#contact" className="nav__link">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div className="nav__toggle" id="nav-toggle">
+                    <i className='bx bx-menu'></i>
+                </div>
+            </nav>
+        </header>
+    </body>
   );
 }
 
