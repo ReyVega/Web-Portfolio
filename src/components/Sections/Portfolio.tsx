3ea1de044a4683dfdc547ee74d9e5b15108eda06
Rey Vega <reynaldovega2010@hotmail.com>
2021-12-12 17:05:07 -0600

    Adds animations to portfolio and active effects

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -1,22 +1,37 @@
-import { RefObject } from "react";
-// import mixitup from 'mixitup';
+import { createRef, RefObject, useLayoutEffect, useRef } from "react";
 import Profile from "../../assets/jojos.png";
 
 interface IPortfolio {
   reference: RefObject<HTMLElement>;
 }
 
-
-// const mixer = mixitup('.portfolio__container', {
-//     selector: {
-//         target: '.portfolio__content',
-//     },
-//     animation: {
-//         duration: 400
-//     }
-// })
+var mixitup = require("mixitup");
 
 function Portfolio(props: IPortfolio) {
+  const portfolio_nav = useRef<HTMLDivElement>(null);
+  const portfolioNavRefs: RefObject<HTMLSpanElement>[] = Array.from(
+    { length: 4 },
+    () => createRef()
+  );
+
+  useLayoutEffect(() => {
+    mixitup(portfolio_nav.current, {
+      selectors: {
+        target: ".portfolio__content",
+      },
+      animation: {
+        duration: 400,
+      },
+    });
+  }, []);
+
+  const activePortfolio = (item: RefObject<HTMLSpanElement>) => {
+    portfolioNavRefs.forEach((ref) => {
+      ref.current?.classList.remove("active-portfolio");
+    });
+    item.current?.classList.add("active-portfolio");
+  };
+
   return (
     <div>
       <section
@@ -28,41 +43,100 @@ function Portfolio(props: IPortfolio) {
         <h2 className="section-title">Portfolio</h2>
 
         <div className="portfolio__nav">
-            <span className="portfolio__item" data-filter="all">All</span>
-            <span className="portfolio__item" data-filer=".web">Web</span>
-            <span className="portfolio__item" data-filter=".app">App</span>
-            <span className="portfolio__item" data-filter=".personal">Personal</span>
+          <span
+            ref={portfolioNavRefs[0]}
+            className="portfolio__item active-portfolio"
+            data-filter="all"
+            onClick={() => activePortfolio(portfolioNavRefs[0])}
+          >
+            All
+          </span>
+          <span
+            ref={portfolioNavRefs[1]}
+            className="portfolio__item"
+            data-filter=".web"
+            onClick={() => activePortfolio(portfolioNavRefs[1])}
+          >
+            Web
+          </span>
+          <span
+            ref={portfolioNavRefs[2]}
+            className="portfolio__item"
+            data-filter=".app"
+            onClick={() => activePortfolio(portfolioNavRefs[2])}
+          >
+            App
+          </span>
+          <span
+            ref={portfolioNavRefs[3]}
+            className="portfolio__item"
+            data-filter=".personal"
+            onClick={() => activePortfolio(portfolioNavRefs[3])}
+          >
+            Personal
+          </span>
         </div>
 
-
-        <div className="portfolio__container bd-grid">
-            <div className="portfolio__content">
-                <a href="/#"><img src={Profile} alt="" className="portfolio__img"/></a>
-                <div className="portfolio__data">
-                    <span className="portfolio__subtitle">Web Development</span>
-                    <a href="/#"><h2 className="portfolio__title">New portfolio item</h2></a>
-                    <a href="/#" className="button button-link">View details</a>
-                </div>
+        <div ref={portfolio_nav} className="portfolio__container bd-grid">
+          <div className="portfolio__content mix web">
+            <a href="/#">
+              <img src={Profile} alt="" className="portfolio__img" />
+            </a>
+            <div className="portfolio__data">
+              <span className="portfolio__subtitle">Web Development</span>
+              <a href="/#">
+                <h2 className="portfolio__title">New portfolio item</h2>
+              </a>
+              <a href="/#" className="button button-link">
+                View details
+              </a>
             </div>
+          </div>
 
-            <div className="portfolio__content">
-                <a href="/#"><img src={Profile} alt="" className="portfolio__img"/></a>
-                <div className="portfolio__data">
-                    <span className="portfolio__subtitle">Web Development</span>
-                    <a href="/#"><h2 className="portfolio__title">New portfolio item</h2></a>
-                    <a href="/#" className="button button-link">View details</a>
-                </div>
+          <div className="portfolio__content mix app">
+            <a href="/#">
+              <img src={Profile} alt="" className="portfolio__img" />
+            </a>
+            <div className="portfolio__data">
+              <span className="portfolio__subtitle">Web Development</span>
+              <a href="/#">
+                <h2 className="portfolio__title">New portfolio item</h2>
+              </a>
+              <a href="/#" className="button button-link">
+                View details
+              </a>
             </div>
+          </div>
 
-            <div className="portfolio__content">
-                <a href="/#"><img src={Profile} alt="" className="portfolio__img"/></a>
-                <div className="portfolio__data">
-                    <span className="portfolio__subtitle">Web Development</span>
-                    <a href="/#"><h2 className="portfolio__title">New portfolio item</h2></a>
-                    <a href="/#" className="button button-link">View details</a>
-                </div>
+          <div className="portfolio__content mix personal">
+            <a href="/#">
+              <img src={Profile} alt="" className="portfolio__img" />
+            </a>
+            <div className="portfolio__data">
+              <span className="portfolio__subtitle">Web Development</span>
+              <a href="/#">
+                <h2 className="portfolio__title">New portfolio item</h2>
+              </a>
+              <a href="/#" className="button button-link">
+                View details
+              </a>
             </div>
+          </div>
 
+          <div className="portfolio__content mix personal">
+            <a href="/#">
+              <img src={Profile} alt="" className="portfolio__img" />
+            </a>
+            <div className="portfolio__data">
+              <span className="portfolio__subtitle">Web Development</span>
+              <a href="/#">
+                <h2 className="portfolio__title">New portfolio item</h2>
+              </a>
+              <a href="/#" className="button button-link">
+                View details
+              </a>
+            </div>
+          </div>
         </div>
       </section>
     </div>
