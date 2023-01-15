d2a5de3c03118ac4a48739b1c4cd63232add70ad
Rey Vega <reynaldovega2010@hotmail.com>
2023-01-15 11:04:40 -0600

    Updates Qualification section and CV

--- src/components/Qualification/data/data.tsx
+++ src/components/Qualification/data/data.tsx
@@ -8,11 +8,18 @@ export interface Item {
 
 export const experienceItems: Item[] = [
   {
+    title: "NXP Semiconductors - DevOps",
+    subtitle: "NXP - Jalisco",
+    iconSubtitle: "bx bx-briefcase-alt",
+    iconDate: "bx bx-calendar-alt",
+    date: "Jan 2023 - Present",
+  },
+  {
     title: "Toshiba Intern - Software Engineer",
     subtitle: "Toshiba GCS - Jalisco",
     iconSubtitle: "bx bx-briefcase-alt",
     iconDate: "bx bx-calendar-alt",
-    date: "Aug 2022 - Jan 2022",
+    date: "Aug 2022 - Jan 2023",
   },
   {
     title: "ITESM - Software Engineer",
